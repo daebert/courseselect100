50f694c16d18b6b8138eca5af2cf9a97bdc8e4a9
daebert <81586917+daebert@users.noreply.github.com>
2021-06-08 13:54:25 +0200

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+//Serve storybook production bundle
+app.use("/storybook", express.static("dist/storybook"));
+
+// Serve app production bundle
+app.use(express.static("dist/app"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}}`);
+});
