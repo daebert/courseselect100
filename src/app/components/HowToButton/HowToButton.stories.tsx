c8d2ec9462563b6f9684570cde5c9ce8d4ea946e
daniel.ebert <daniel.ebert@web.de>
2021-06-11 22:25:46 +0200

    ✨ Build HowToButton

---
+++ src/app/components/HowToButton/HowToButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./HowToButton";
+
+export default {
+  title: "Component/HowToButton",
+  component: Button,
+};
+
+export const HowToButton = (): JSX.Element => (
+  <Button>Wie es funktioniert!</Button>
+);
