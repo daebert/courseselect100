c849af57b271393a7e640b5a9568bf8f291cd6e9
daniel.ebert <daniel.ebert@web.de>
2021-06-11 12:33:55 +0200

    :fire: remove prio ball component

--- src/app/components/PrioBall/PrioBall.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type PrioIconProps = {
-  icon: JSX.Element;
-};
-
-export default function PrioIcon({ icon }: PrioIconProps): JSX.Element {
-  return <div>{icon}</div>;
-}
