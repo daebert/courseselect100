c76d9b3c848fd8ee1a7be6355f8a2476bcd154d4
daniel.ebert <daniel.ebert@web.de>
2021-06-12 11:31:26 +0200

    ✨ Build ClassBall svg

---
+++ src/app/assets/icons/ClassBallDoubleDigit.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type ClassBallProps = {
+  num: string;
+};
+
+export default function ClassBallDoubleDigit({
+  num,
+}: ClassBallProps): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+    >
+      <g
+        fill="var(--primary-color)"
+        stroke="var(--primary-color)"
+        stroke-width="1"
+      >
+        <circle cx="12.5" cy="12.5" r="12.5" stroke="none" />
+        <circle cx="12.5" cy="12.5" r="12" fill="none" />
+      </g>
+      <text
+        transform="translate(12 19)"
+        fill="var(--contrast-text-color)"
+        font-size="16"
+        font-family="Roboto-Regular, Roboto"
+        letter-spacing="0.025em"
+      >
+        <tspan x="-9.184" y="0">
+          {num}
+        </tspan>
+      </text>
+    </svg>
+  );
+}
---
+++ src/app/assets/icons/ClassBallNo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ClassBallNo(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+    >
+      <g fill="none" stroke="var(--primary-color)" stroke-width="1">
+        <circle cx="12.5" cy="12.5" r="12.5" stroke="none" />
+        <circle cx="12.5" cy="12.5" r="12" fill="none" />
+      </g>
+    </svg>
+  );
+}
