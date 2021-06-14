21093e4df11bb410b92348b606b88fb5c64bf253
daniel.ebert <daniel.ebert@web.de>
2021-06-14 18:53:54 +0200

    ♻️ Refactor MainButton and start with CSS

--- src/app/components/MainButton/MainButton.test.tsx
+++ src/app/components/MainButton/MainButton.test.tsx
@@ -1,11 +1,11 @@
-// import React from "react";
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import MainButton from "./MainButton";
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import MainButton from "./MainButton";
 
-// test("renders Start Button", () => {
-//   render(<MainButton choice={false}>Senden</MainButton>);
-//   expect(screen.getByRole("button").innerHTML).toContain("Senden");
-// });
+test("renders Start Button", () => {
+  render(<MainButton choice={false}>Senden</MainButton>);
+  expect(screen.getByRole("button").innerHTML).toContain("Senden");
+});
 
 // test("calls onClick", () => {
 //   const handleClick = jest.fn();
