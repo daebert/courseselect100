941df2a47a29623cd1cc5a9fc9923da5472a1566
daniel.ebert <daniel.ebert@web.de>
2021-06-15 15:51:08 +0200

    🚚 create page/home folder and put page files into this folder

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import CourseChoiceBlock from "../components/CourseChoiceBlock/CourseChoiceBlock";
-import HowToButton from "../components/HowToButton/HowToButton";
-import MainButton from "../components/MainButton/MainButton";
+import CourseChoiceBlock from "../../components/CourseChoiceBlock/CourseChoiceBlock";
+import HowToButton from "../../components/HowToButton/HowToButton";
+import MainButton from "../../components/MainButton/MainButton";
 import styles from "./Home.module.css";
 
 function Home(): JSX.Element {
