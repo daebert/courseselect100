f51c6147d02b5d6203124bbddf8763e3528bfb37
daniel.ebert <daniel.ebert@web.de>
2021-06-16 14:29:15 +0200

    ✨ Connect to MongoDB

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,3 +1,11 @@
+import { MongoClient } from "mongodb";
+
 export const connectDatabase = async (url: string): Promise<void> => {
-  console.log(url);
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const databasesList = await client.db().admin().listDatabases();
+  console.log(databasesList);
 };
