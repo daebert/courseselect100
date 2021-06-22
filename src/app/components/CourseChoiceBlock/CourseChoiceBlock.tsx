aa26270da46f69ca77844696d6c749f689a13e03
daniel.ebert <daniel.ebert@web.de>
2021-06-22 16:30:14 +0200

    ✨ Get data and parse data from localStroage

--- src/app/components/CourseChoiceBlock/CourseChoiceBlock.tsx
+++ src/app/components/CourseChoiceBlock/CourseChoiceBlock.tsx
@@ -2,15 +2,57 @@ import React from "react";
 import CourseChoice from "../CourseChoice/CourseChoice";
 import styles from "./CourseChoiceBlock.module.css";
 
-const retrievedObject = localStorage.getItem("choiceObject");
-console.log(retrievedObject);
+// const choiceObject = localStorage.getItem("choiceObject");
+// console.log(choiceObject);
+
+// if (choiceObject !== null) {
+//   JSON.parse(choiceObject);
+// } else {
+//   ("");
+// }
+
+type Choice = {
+  primary: {
+    name: string;
+  };
+  secondary: {
+    name: string;
+  };
+  tertiary: {
+    name: string;
+  };
+};
+
+function parseChoiceFromLocalStorage(): Choice {
+  const json = localStorage.getItem("choiceObject");
+  if (json === null) {
+    return {
+      primary: {
+        name: "",
+      },
+      secondary: {
+        name: "",
+      },
+      tertiary: {
+        name: "",
+      },
+    };
+  }
+  const data = JSON.parse(json);
+  return data;
+}
+
+const PrioName = parseChoiceFromLocalStorage();
+const name1 = PrioName.primary === undefined ? "" : PrioName.primary.name;
+const name2 = PrioName.secondary === undefined ? "" : PrioName.secondary.name;
+const name3 = PrioName.tertiary === undefined ? "" : PrioName.tertiary.name;
 
 function CourseChoiceBlock(): JSX.Element {
   return (
     <div className={styles.courseChoiceBlock}>
-      <CourseChoice priority="primary" name="Tanzen" />
-      <CourseChoice priority="secondary" name="" />
-      <CourseChoice priority="tertiary" name="" />
+      <CourseChoice priority="primary" name={name1} />
+      <CourseChoice priority="secondary" name={name2} />
+      <CourseChoice priority="tertiary" name={name3} />
     </div>
   );
 }
