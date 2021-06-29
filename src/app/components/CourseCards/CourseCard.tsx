36abf5c505514d4d1196d8c5499eb14ffb6f49b5
daniel.ebert <daniel.ebert@web.de>
2021-06-29 15:38:08 +0200

    💄 refactor course Card

--- src/app/components/CourseCards/CourseCard.tsx
+++ src/app/components/CourseCards/CourseCard.tsx
@@ -5,14 +5,12 @@ import styles from "./CourseCard.module.css";
 type CourseCardProps = {
   imgSrc: string;
   headline: string;
-  body: string;
   priority: string | null;
   coursename: string;
 };
 
 function CourseCard({
   headline,
-  body,
   imgSrc,
   priority,
   coursename,
@@ -25,7 +23,6 @@ function CourseCard({
       <div className={styles.courseCard}>
         <img className={styles.courseCard__img} src={imgSrc} />
         <h3 className={styles.courseCard__headline}>{headline}</h3>
-        <span className={styles.courseCard__body}>{body}</span>
       </div>
     </Link>
   );
