c3d3945af157ce94297b8b158e1e8c40021a53fd
daniel.ebert <daniel.ebert@web.de>
2021-07-02 10:18:20 +0200

    ✨ Change Link into history.push inside a button

--- src/app/components/CourseCards/CourseCard.tsx
+++ src/app/components/CourseCards/CourseCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./CourseCard.module.css";
+import { useHistory } from "react-router-dom";
 
 type CourseCardProps = {
   imgSrc: string;
@@ -17,15 +18,17 @@ function CourseCard({
   title,
   disabled,
 }: CourseCardProps): JSX.Element {
+  const history = useHistory();
   return (
-    <button className={styles.courseCard__link} disabled={disabled}>
-      <Link
-        to={`/detail?priority=${priority}&title=${title}`}
-        className={styles.courseCard}
-      >
-        <img className={styles.courseCard__img} src={imgSrc} />
-        <h3 className={styles.courseCard__headline}>{headline}</h3>
-      </Link>
+    <button
+      className={styles.courseCard__link}
+      disabled={disabled}
+      onClick={() =>
+        history.push(`/detail?priority=${priority}&title=${title}`)
+      }
+    >
+      <img className={styles.courseCard__img} src={imgSrc} />
+      <h3 className={styles.courseCard__headline}>{headline}</h3>
     </button>
   );
 }
